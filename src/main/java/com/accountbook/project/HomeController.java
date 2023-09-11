d1706db69ae177138aa29836410470c09c6bc2c0
hobbyscripterII <wruomma2@naver.com>
2023-09-11 18:59:08 +0900

    :sparkles: 인터셉터 적용

--- src/main/java/com/accountbook/project/HomeController.java
+++ src/main/java/com/accountbook/project/HomeController.java
@@ -1,15 +1,17 @@
 package com.accountbook.project;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
+@Slf4j
 @Controller
 public class HomeController {
     @GetMapping("/")
-    public String home(HttpServletRequest request) {
+    public String home(HttpServletRequest request) throws Exception {
         HttpSession session = request.getSession(false);
         return "home";
     }
---
+++ src/main/java/com/accountbook/project/interceptor/LogInterceptor.java
@@ -0,0 +1,49 @@
+package com.accountbook.project.interceptor;
+
+import com.accountbook.project.SessionConst;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.net.Inet4Address;
+
+@Slf4j
+public class LogInterceptor implements HandlerInterceptor {
+    public static final String IP = "USER_IP";
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        String url = request.getRequestURI();
+        String ip = Inet4Address.getLocalHost().getHostAddress();
+        request.setAttribute(IP, ip);
+        log.info("REQUEST [{}][{}]", ip, url);
+
+        HttpSession session = request.getSession(false);
+
+        if(session == null || session.getAttribute(SessionConst.MEMBER_ID) == null) {
+            log.info("미인증 사용자 요청 [{}][{}]", ip, url);
+            response.sendRedirect("/accountbook/login");
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+        log.info("POSTHANDLE [{}]", modelAndView);
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+        String url = request.getRequestURI();
+        String ip = (String)request.getAttribute(IP);
+        log.info("RESPONSE [{}][{}]", ip, url);
+
+        if(ex != null)
+            log.error("AFTERCOMPLETION ERROR", ex);
+    }
+}
