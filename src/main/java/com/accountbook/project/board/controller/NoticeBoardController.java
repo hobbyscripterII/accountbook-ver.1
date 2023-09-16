b6151d7176ca37db1d8e5f52093915c450382a59
hobbyscripterII <wruomma2@naver.com>
2023-09-16 10:58:11 +0900

    :bug: 공지사항 비회원 접근 불가 수정

--- src/main/java/com/accountbook/project/board/controller/NoticeBoardController.java
+++ src/main/java/com/accountbook/project/board/controller/NoticeBoardController.java
@@ -2,6 +2,7 @@ package com.accountbook.project.board.controller;
 
 import com.accountbook.project.SessionConst;
 import com.accountbook.project.board.dto.BoardDto;
+import com.accountbook.project.board.service.BoardCommentService;
 import com.accountbook.project.board.service.BoardService;
 import com.accountbook.project.pagination.PaginationDto;
 import lombok.RequiredArgsConstructor;
@@ -22,6 +23,7 @@ import java.util.Map;
 @RequiredArgsConstructor
 public class NoticeBoardController {
     private final BoardService boardService;
+    private final BoardCommentService boardCommentService;
 
     @GetMapping("/list")
     public String board(Model model, @RequestParam(defaultValue = "1") int page) {
@@ -34,12 +36,12 @@ public class NoticeBoardController {
     }
 
     @GetMapping("/list/{b_id}")
-    public String board(@PathVariable(name = "b_id") int b_id, Model model, HttpServletRequest request) {
+    public String board(@PathVariable(name = "b_id") int b_id, Model model) {
         boardService.updateContentCnt(b_id);
         Map<String, Object> map = new HashMap<String, Object>();
         map.put("b_id", b_id);
         map.put("b_code", 1);
-        boardService.getBoard(b_id, model, request, map);
+        getBoard(b_id, model, map);
         boardInfo(model);
         return "board/board-read";
     }
@@ -83,6 +85,12 @@ public class NoticeBoardController {
         board.setN_title(n_title);
         board.setN_content(n_content);
         boardService.updateContent(board);
+    }
+
+    public void getBoard(int b_id, Model model, Map<String, Object> map) {
+        model.addAttribute("list", boardService.selectContent(map));
+        model.addAttribute("flag", boardService.modifyFlag(b_id));
+        model.addAttribute("comment", boardCommentService.getComment(b_id));
     }
 
     private static void boardInfo(Model model) {
